cfa1b49ce933e6a72e6431f4043f8b6933407d5e
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2023-08-28 13:59:47 +0300

    Fix linters

--- doctor_appointment_backend/db/migrate/20230828102437_create_join_table_appointments.rb
+++ doctor_appointment_backend/db/migrate/20230828102437_create_join_table_appointments.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 class CreateJoinTableAppointments < ActiveRecord::Migration[7.0]
   def change
     create_join_table :users, :doctors do |t|
-      t.index [:user_id, :doctor_id]
-      t.index [:doctor_id, :user_id]
+      t.index %i[user_id doctor_id]
+      t.index %i[doctor_id user_id]
       t.string :city
       t.date :appontment_date
     end
