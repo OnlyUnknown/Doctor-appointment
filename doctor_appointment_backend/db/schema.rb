fa07780ea87b56c3944998915acd702f7d6aeeae
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2023-08-28 18:23:08 +0300

    Add devise to users

--- doctor_appointment_backend/db/schema.rb
+++ doctor_appointment_backend/db/schema.rb
@@ -10,32 +10,42 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_230_828_102_437) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_28_151712) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'doctors', force: :cascade do |t|
-    t.string 'name'
-    t.string 'speciality'
-    t.text 'description'
-    t.float 'consultation_fees'
-    t.integer 'years_of_experience'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "doctors", force: :cascade do |t|
+    t.string "name"
+    t.string "speciality"
+    t.text "description"
+    t.float "consultation_fees"
+    t.integer "years_of_experience"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'doctors_users', id: false, force: :cascade do |t|
-    t.bigint 'user_id', null: false
-    t.bigint 'doctor_id', null: false
-    t.string 'city'
-    t.date 'appontment_date'
-    t.index %w[doctor_id user_id], name: 'index_doctors_users_on_doctor_id_and_user_id'
-    t.index %w[user_id doctor_id], name: 'index_doctors_users_on_user_id_and_doctor_id'
+  create_table "doctors_users", id: false, force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "doctor_id", null: false
+    t.string "city"
+    t.date "appontment_date"
+    t.index ["doctor_id", "user_id"], name: "index_doctors_users_on_doctor_id_and_user_id"
+    t.index ["user_id", "doctor_id"], name: "index_doctors_users_on_user_id_and_doctor_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
 end
