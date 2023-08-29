cbdf68a27afe3f5cd25d5bb2276fec8392b07e68
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2023-08-29 20:36:30 +0300

    Update user model & routes

--- doctor_appointment_backend/config/routes.rb
+++ doctor_appointment_backend/config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'users/index'
+      post 'users/create'
+      get '/show/:id', to: 'users#show'
+      delete '/destroy/:id', to: 'users#destroy'
+    end
+  end
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
