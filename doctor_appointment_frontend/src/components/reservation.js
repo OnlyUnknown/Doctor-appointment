0d3f69031bf8b2696dffb395a725c933f845a32f
touredri <dcompte223@gmail.com>
2023-08-29 07:55:26 +0100

    reservation page styling

--- doctor_appointment_frontend/src/components/reservation.js
+++ doctor_appointment_frontend/src/components/reservation.js
@@ -1,9 +1,14 @@
 import  React from 'react';
+import '../css/home.css'
+import doc5 from './doc5.jpg'
 
 const Reservation = () => {
     return (
-        <div>
-            <h1>Reservation</h1>
+        <div className="reservation-container h-screen" style={{backgroundImage: `url(${doc5})`, backgroundSize: 'cover'}}>
+          <div className="w-full h-screen reservation flex flex-col justify-center items-center text-white">
+            <h1 className="text-3xl tracking-2xl font-bold space-x-1 mb-6">MAKE AN APPOINTMENT WITH A DOCTOR </h1>
+            <hr className="text-white w-3/4" />
+          </div>
         </div>
     );
 }
