d76639a246f37656d868533c3f9e35a93f17e968
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2023-08-29 21:05:43 +0300

    Fix linter issues

--- doctor_appointment_backend/db/schema.rb
+++ doctor_appointment_backend/db/schema.rb
@@ -12,7 +12,6 @@
 
 
 ActiveRecord::Schema[7.0].define(version: 20_230_829_130_458) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
