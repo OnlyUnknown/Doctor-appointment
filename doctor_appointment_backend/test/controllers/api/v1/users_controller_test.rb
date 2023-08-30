2f98479ba06c80ab8b5999e85b4bfd69a1beb0d3
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2023-08-30 14:32:37 +0300

    Fix linters

--- doctor_appointment_backend/test/controllers/api/v1/users_controller_test.rb
+++ doctor_appointment_backend/test/controllers/api/v1/users_controller_test.rb
@@ -1,22 +1,22 @@
-require "test_helper"
+require 'test_helper'
 
 class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get api_v1_users_index_url
     assert_response :success
   end
 
-  test "should get create" do
+  test 'should get create' do
     get api_v1_users_create_url
     assert_response :success
   end
 
-  test "should get show" do
+  test 'should get show' do
     get api_v1_users_show_url
     assert_response :success
   end
 
-  test "should get destroy" do
+  test 'should get destroy' do
     get api_v1_users_destroy_url
     assert_response :success
   end
