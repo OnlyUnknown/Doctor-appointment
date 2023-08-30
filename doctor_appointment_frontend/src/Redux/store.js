63ac835e2fea0e2f618e606c0a879ee3bd9f3088
Abdullah <abdullahkhrief@gmail.com>
2023-08-30 17:00:09 +0300

    have added store .js

---
+++ doctor_appointment_frontend/src/Redux/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import appointmentReducer from "./feature/appointmentSlice"
+
+
+const store = configureStore({
+    reducer: {
+        appointment: appointmentReducer,
+    }
+})
+
+export default store
