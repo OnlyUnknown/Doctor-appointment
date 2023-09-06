b7a177247983790bda1270b9e6b2c18eedf7ade8
Abdullah <abdullahkhrief@gmail.com>
2023-09-06 17:41:12 +0300

    have created svgs components for cleaner code

--- doctor_appointment_frontend/src/components/add.js
+++ doctor_appointment_frontend/src/components/add.js
@@ -3,6 +3,7 @@ import '../css/home.css';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import doc5 from './doc5.jpg';
+import AddSVG from './svgs/AddSVG';
 
 function DoctorForm() {
   const navigate = useNavigate();
@@ -172,17 +173,7 @@ function DoctorForm() {
             type="button"
             className="absolute bottom-4 left-0 w-16 h-10 flex invisible sm:visible  justify-center items-center rounded-tr-full rounded-br-full border border-amber-50 focus:bg-[#97bf0f] shadow-md"
           >
-            <svg
-              className="h-4 w-4 text-gray-800 dark:text-white"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="white"
-              viewBox="0 0 10 16"
-            >
-              <path
-                d="M8.766.566A2 2 0 0 0 6.586 1L1 6.586a2 2 0 0 0 0 2.828L6.586 15A2 2 0 0 0 10 13.586V2.414A2 2 0 0 0 8.766.566Z"
-              />
-            </svg>
+            <AddSVG />
           </button>
         </Link>
       </div>
---
+++ doctor_appointment_frontend/src/components/svgs/Arrowicon.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function ArrowIcon() {
+  return (
+    <svg
+      className="h-4 w-4 text-gray-800 dark:text-white"
+      aria-hidden="true"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="white"
+      viewBox="0 0 10 16"
+    >
+      <path
+        d="M8.766.566A2 2 0 0 0 6.586 1L1 6.586a2 2 0 0 0 0 2.828L6.586 15A2 2 0 0 0 10 13.586V2.414A2 2 0 0 0 8.766.566Z"
+      />
+    </svg>
+  );
+}
+
+export default ArrowIcon;
