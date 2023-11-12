e28829d36c98aa48c895109dd275fc431ed837c3
OnlyUnknown <abdullahkhrief@gmail.com>
2023-11-12 23:06:10 +0300

    have added the render buiold file

--- doctor_appointment_backend/bin/birds-build.sh
+++ doctor_appointment_backend/bin/birds-build.sh
@@ -5,5 +5,5 @@ set -o errexit
 bundle install
 # bundle exec rake assets:precompile # These lines are commented out because we have an API only app
 # bundle exec rake assets:clean
-bundle exec rake db:migrate
-bundle exec rake db:seed
+rake db:migrate
+rake db:seed
